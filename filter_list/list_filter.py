3397640fbd0567debe70d7627b4fba07287abbc5
nawal <nawal.bhengra@stackmybiz.com>
2022-03-09 17:32:47 +0530

    list comprehension

---
+++ filter_list/list_filter.py
@@ -0,0 +1,7 @@
+from list_filter_example.filter_list import lists_class_filter
+
+a:int = 2
+
+filter = lists_class_filter.filter_list_value(a)
+
+print(f"filterd values of List {filter}")
---
+++ filter_list/list_filter_example/filter_list.py
@@ -0,0 +1,9 @@
+class lists_class_filter:
+    
+    def filter_list_value(a):
+        list_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        print(f"all values {list_values}")
+
+        new_list = [i for i in list_values if i % a == 0]
+
+        return new_list
