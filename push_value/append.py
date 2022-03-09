11adcc39d6b957b764d140938007ee5a7a6ce7ec
nawal <nawal.bhengra@stackmybiz.com>
2022-03-09 21:03:15 +0530

    append lists

---
+++ push_value/append.py
@@ -0,0 +1,5 @@
+from append_list.append_list import Append_list
+
+class_instance = Append_list()
+result_value = class_instance.list_data()
+print(f"new list using append {result_value}")
---
+++ push_value/append_list/append_list.py
@@ -0,0 +1,6 @@
+class Append_list:
+    list_items = ["python", "Js", "ReactJs", "NextJs", "React-Native", "Flutter", "Php", "NodeJs"]
+    
+    def list_data(self):
+        self.new_list = [i for i in self.list_items if "Js" in i ]
+        return self.new_list
